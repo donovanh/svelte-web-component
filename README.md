192497eab91d151f4d83c83ff2c73e51ec3b3674
Donovan Hutchinson <donovanh@gmail.com>
2019-12-12 15:42:41 +0000

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Svelte Web Component example
+
+A super-simple Web Component created using Svelte.
---
+++ src/index.svelte
@@ -0,0 +1,9 @@
+<script>
+
+export let name = 'World';
+
+</script>
+
+<svelte:options tag='hello-world' />
+
+<h2>Hello {name}</h2>
